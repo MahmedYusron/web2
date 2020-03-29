5fe661c82c2c08724997a11c29d5351117ea68bf
MahmedYusron <m.yusron1498@gmail.com>
2020-03-29 19:38:52 +0700

    pertemuan5

---
+++ 05Latihan1.php
@@ -0,0 +1,10 @@
+<?php
+
+$oldText = "Hello World!";
+$newText = str_replace("World", "Dolly", $oldText) ;
+
+echo $newText;
+echo"<br>";
+echo date('l, d-m-Y');
+
+?>
---
+++ 05Latihan2.php
@@ -0,0 +1,9 @@
+<?php
+$myfile = ...("test1.txt", "r");
+
+while(!...($myfile)) {
+
+  echo ...($myfile);
+
+}
+>?
